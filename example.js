bce3e98a9707182bd647c27b019fe37e06cdb173
Anders Rune Jensen <arj03@protonmail.ch>
2018-09-21 22:32:22 +0200

    Support pretty-printed

--- example.js
+++ example.js
@@ -1,37 +1,5 @@
 const compile = require('./')
 
-/*
-const sbotJSON = 
-{
-  "key": "%R7lJEkz27lNijPhYNDzYoPjM0Fp+bFWzwX0SmNJB/ZE=.sha256",
-  "value": {
-    "previous": null,
-    "author": "@FCX/tsDLpubCPKKfIrw4gc+SQkHcaD17s7GI6i/ziWY=.ed25519",
-    "sequence": 2,
-    "timestamp": 1514517078157,
-    anyOf: [
-      { arbitraryJSON: { "content": "arbitrary-json" } },
-      { "content": "box" }
-    ],
-    "hash": "sha256",
-    "signature": "z7W1ERg9UYZjNfE72ZwEuJF79khG+eOHWFp6iF+KLuSrw8Lqa6IousK4cCn9T5qFa8E14GVek4cAMmMbjqDnAg==.sig.ed25519"
-  },
-  "timestamp": 1514517078160
-}
-
-// multiple anyof?
-
-    anyOf: [
-      { "previous": "%XphMUkWQtomKjXQvFGfsGYpt69sgEY7Y4Vou9cEuJho=.sha256" },
-      { "previous": null }
-    ],
-
-    anyOf: [
-      { arbitraryJSON: { "content": "arbitrary-json" } },
-      { "content": "box" }
-    ],
-*/
-
 const sbotSchema = {
   type: 'object',
   properties: {
@@ -65,7 +33,7 @@ const sbotSchema = {
 }
 
 // pass in a type schema
-const parse = compile(sbotSchema)
+const parse = compile(sbotSchema, { prettyPrinted: true })
 
 const sbotExample = 
 {
@@ -86,16 +54,53 @@ const sbotExample =
   "timestamp": 1526161604274
 }
 
+const sbotExample2 = 
+{
+  "key": "%H3MlLmVPVgHU6rBSzautUBZibDttkI+cU4lAFUIM8Ag=.sha256",
+  "value": {
+    "previous": "somthing",
+    "author": "@6CAxOI3f+LUOVrbAl0IemqiS7ATpQvr9Mdw9LC4+Uv0=.ed25519",
+    "sequence": 1,
+    "timestamp": 1456154790701,
+    "hash": "sha256",
+    "content": "box",
+    "signature": "O37i6PCVZltV6Jm1MON7BzLbKCbe/Qxe3o49tiO0PUOC8q+qtWOT94Zppn6E7R9RzHVnlv47IwiKwOnVibgJBg==.sig.ed25519"
+  },
+  "timestamp": 1526161604274
+}
+
+const ex3 =
+{
+  "key": "%R0wZB82B2scyS6LaIuiIvk9E1s3rucEdvw4ClC/9RNw=.sha256",
+  "value": {
+    "previous": "%Es/iGfhScjop6scrM7jdHkJlQFLJWW98M7xEhZzULpo=.sha256",
+    "author": "@TXKFQehlyoSn8UJAIVP/k2BjFINC591MlBC2e2d24mA=.ed25519",
+    "sequence": 113,
+    "timestamp": 1466648384789,
+    "hash": "sha256",
+    "content": {
+      "type": "post",
+      "text": "Hey having some trouble with a weird network setup in China, had to hardcode 'localhost' into here: \n\n    var oracle = {\n    getHostname: function () {\n      return 'localhost'\n    }\n\nElse it'd try serving on:\n\n    Starting...\n    Loading plugins from /Users/joran/.ssb/node_modules\n    Log level: notice\n    Serving at http://1.3.3.48:7777\n\nWith network config:\n\n    lo0: flags=8049<UP,LOOPBACK,RUNNING,MULTICAST> mtu 16384\n\toptions=3<RXCSUM,TXCSUM>\n\tinet6 ::1 prefixlen 128\n\tinet 127.0.0.1 netmask 0xff000000\n\tinet6 fe80::1%lo0 prefixlen 64 scopeid 0x1\n\tnd6 options=1<PERFORMNUD>\n    gif0: flags=8010<POINTOPOINT,MULTICAST> mtu 1280\n    stf0: flags=0<> mtu 1280\n    en1: flags=8863<UP,BROADCAST,SMART,RUNNING,SIMPLEX,MULTICAST> mtu 1500\n\tether 60:33:4b:27:aa:6f\n\tinet6 fe80::6233:4bff:fe27:aa6f%en1 prefixlen 64 scopeid 0x4\n\tinet 1.3.3.48 netmask 0xffffff00 broadcast 1.3.3.255\n\tnd6 options=1<PERFORMNUD>\n\tmedia: autoselect\n\tstatus: active\n    en0: flags=8863<UP,BROADCAST,SMART,RUNNING,SIMPLEX,MULTICAST> mtu 1500\n\toptions=b<RXCSUM,TXCSUM,VLAN_HWTAGGING>\n\tether c4:2c:03:39:d7:91\n\tnd6 options=1<PERFORMNUD>\n\tmedia: autoselect (none)\n\tstatus: inactive\n    fw0: flags=8863<UP,BROADCAST,SMART,RUNNING,SIMPLEX,MULTICAST> mtu 4078\n\tlladdr 78:ca:39:ff:fe:11:ee:9e\n\tnd6 options=1<PERFORMNUD>\n\tmedia: autoselect <full-duplex>\n\tstatus: inactive\n    p2p0: flags=8843<UP,BROADCAST,RUNNING,SIMPLEX,MULTICAST> mtu 2304\n\tether 02:33:4b:27:aa:6f\n\tmedia: autoselect\n\tstatus: inactive",
+      "channel": "patchwork-dev"
+    },
+    "signature": "aPhXwQiPO8nsB3/aaU91KmamnAjjvk3PmZ5N+7ARIPOPhn/hmwBRQEkfJJV199x2vKStvaf8r/F+ZGhCxQPADw==.sig.ed25519"
+  },
+  "timestamp": 1526161668580
+}
+
 /*
 for (var i = 0; i < 100; ++i)
   console.log(ex[i], ex.charCodeAt(i))
  */
 
 // will return {hello: 'world'}
-console.log(parse.toString())
-console.log(parse(JSON.stringify(sbotExample)))
+//console.log(parse.toString())
+//console.log(parse(JSON.stringify(sbotExample)))
 //console.log(ex)
 //console.log(ex.replace(/[ \t\r\n]+/g,""))
-const ex = JSON.stringify(sbotExample, null, 2)
-//console.log(parse(ex.replace(/[ \t\r\n]+/g,"")))
+const ex = JSON.stringify(sbotExample2, null, 2)
+console.log(ex)
+console.log(JSON.stringify(sbotExample2))
+console.log(parse(ex)) // .replace(/[ \t\r\n]+/g,"")
+//console.log(ex3.replace(/[ \t\r\n]+/g,""))
 
